dda1b85e5c59364d311ca3ba076484007f70e575
Laura Saporski <laaurasiqueira@hotmail.com>
2025-04-26 17:54:26 -0300

    .

--- data.py
+++ data.py
@@ -1,10 +1,7 @@
 # Arquivo com constantes
-# Arquivo com constantes
-# Para revisores: adicionado por estudantes no S7
 
 
-# A URL é substituída pela URL do servidor gerada pela plataforma
-URBAN_ROUTES_URL = 'https://cnt-dc0792fa-ff76-4dfc-ae38-1a6225f8fada.containerhub.tripleten-services.com?lng=pt'
+URBAN_ROUTES_URL = 'https://cnt-995249e9-62fd-4cda-a20f-271536a5fbd5.containerhub.tripleten-services.com/?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
