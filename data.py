78a591ab80ac14875ab3e9d08841807ddc523d20
Laura Saporski <laaurasiqueira@hotmail.com>
2025-04-26 18:54:38 -0300

    .

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 # Arquivo com constantes
 
 
-URBAN_ROUTES_URL = 'https://cnt-995249e9-62fd-4cda-a20f-271536a5fbd5.containerhub.tripleten-services.com/?lng=pt'
+URBAN_ROUTES_URL = 'https://cnt-891aac96-c4e8-4f1a-b850-244390b04b60.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
